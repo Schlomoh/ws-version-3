2f79abdef6f6db0e48dd0ff24994847e11e3df43
Mo <71161651+Schlomoh@users.noreply.github.com>
2021-12-13 18:10:53 +0100

    init img component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import { NextPage } from "next";
+import Image from 'next/image'
 import { PageHead } from "../src/components/Head";
 
 // layout
 import ContentRow from "../src/components/layout/ContentRow";
+import { StPicture } from "../src/components/layout/Picture/picStyle";
 
 // values
 import values from "../src/constants/strings.json";
+
+import img from '../src/assets/img.jpg'
 
 const HomePage: NextPage = () => {
   const text = values.home;
@@ -31,8 +35,7 @@ const HomePage: NextPage = () => {
             <h3>{text.greet.subHeading}</h3>
           </>
           <>
-            <h1>{text.greet.heading}</h1>
-            <h3>{text.greet.subHeading}</h3>
+    <Image src={img} alt="" layout="fill"/>
           </>
         </ContentRow>
         <ContentRow bgColor="white">
--- src/components/layout/Picture/picStyle.tsx
+++ src/components/layout/Picture/picStyle.tsx
@@ -1,7 +1,10 @@
 import styled from 'styled-components'
 
 export const StPicture = styled.div`
+    width: 100%;
+    max-height: 100vh;
     img{
-        
+        width: 100%;
+        height: 100%;
     }
 `
