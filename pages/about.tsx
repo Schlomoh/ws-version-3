70a72750bbb19587915517c1e548669cabb5c101
Schlomoh <moritz@weltfern.com>
2022-03-22 03:05:37 +0100

    cleanup error fix

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,14 +2,7 @@ import BasePage from "../components/basePage";
 import background from "../assets/img/background.jpg";
 
 const About = () => {
-  return (
-    <BasePage
-      image={background}
-      title="about"
-      subtitle="You're on the"
-      Content={() => <div></div>}
-    />
-  );
+  return <BasePage render={<div></div>} />;
 };
 
 export default About;
