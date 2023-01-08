1b7fd127503f0397eb70e7ea54796e8ee6c915cf
post@moritzbecker.de <post@moritzbecker.de>
2023-01-08 23:35:35 +0100

    article image overflow fix

--- components/Styled/articleImage.tsx
+++ components/Styled/articleImage.tsx
@@ -2,8 +2,10 @@ import styled from "styled-components";
 
 const ArticleImage = styled.div`
   width: 100%;
-  height: 500px;
+  height: 300px;
+  max-height: 30vh;
   border-radius: ${props => props.theme.misc.borderRadius.outer};
+  overflow: hidden;
   
   img {
     object-fit: cover;
