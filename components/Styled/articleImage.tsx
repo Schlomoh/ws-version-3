e543b0569e82dc61fde4b5c04733d8d80532c058
post@moritzbecker.de <post@moritzbecker.de>
2023-01-08 23:17:15 +0100

    fix article image

--- components/Styled/articleImage.tsx
+++ components/Styled/articleImage.tsx
@@ -2,9 +2,15 @@ import styled from "styled-components";
 
 const ArticleImage = styled.div`
   position: relative;
-  left: -100px; // offset
-  width: calc(100% + 100px + 30px); // offset + padding
-  margin: 30px 0;
+  width: 100%;
+  height: 500px;
+  border-radius: ${props => props.theme.misc.borderRadius.outer}
+  
+  img {
+    object-fit: cover;
+    height: 100%;
+    width: 100%
+  }
 `;
 
 export default ArticleImage;
